87084f16a29e2d7a669584eb7def663eacd6c561
Katie Copeland <K.COPELAND-2018@hull.ac.uk>
2020-10-08 11:47:21 +0100

    Added an input file.

--- LabExercise01/Program.cs
+++ LabExercise01/Program.cs
@@ -6,7 +6,7 @@ namespace LabExercise01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello again, World!");
         }
     }
 }
